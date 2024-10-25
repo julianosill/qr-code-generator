ea7f806a5a35ce1d9f98444b473f1d85ec62ffb0
Juliano Sill <sill.juliano@gmail.com>
2024-10-25 17:46:50 -0300

    build: setup eslint import sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,20 @@
-import globals from 'globals'
 import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default [
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
     rules: {
       semi: ['error', 'never'],
-      quotes: ['error', 'single']
+      quotes: ['error', 'single'],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     }
   },
   {languageOptions: { globals: globals.browser }},
