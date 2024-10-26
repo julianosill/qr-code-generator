d9d63cd7313adbc39ffaa0b193b49cb0848e6185
Juliano Sill <sill.juliano@gmail.com>
2024-10-26 00:30:39 -0300

    build: confit airbnb and prettier eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,33 @@
 import pluginJs from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 import pluginReact from 'eslint-plugin-react'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default [
+  eslintConfigPrettier,
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     plugins: {
+      prettier: eslintPluginPrettier,
       'simple-import-sort': simpleImportSort,
     },
+    settings: {
+      react: { version: 'detect' },
+    },
+    languageOptions: {
+      globals: globals.browser,
+    },
     rules: {
-      semi: ['error', 'never'],
-      quotes: ['error', 'single'],
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
-    }
+    },
   },
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ]
---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+export default {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  jsxSingleQuote: true,
+  semi: false,
+  singleAttributePerLine: false,
+  singleQuote: true,
+  tabWidth: 2,
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['clsx', 'tw', 'cva'],
+}
